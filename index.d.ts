af25051555e1509daa73d5b73985de42dd7295b7
Manoel Fernandes <manoel-fernandes@outlook.com>
2025-06-07 15:42:28 -0300

    silent argument fixed on index.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ export class Validate{
 	check(content: any, type: string): boolean;
 	checkOptions(value: any, content: any[]): boolean;
 	checkRange(value: number | bigint, range: { from: number | bigint, to: number | bigint }): boolean;
-	silent(value: boolean): void;
+	silent(mode: boolean): void;
 	isSilent(): boolean;
 	getType(input: any): string;
 	getErrors(): string[];
